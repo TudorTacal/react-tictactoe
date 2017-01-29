ff66be8c2a625160b8fd97341e1cb161c6e768ec
TudorTacal <tudortacal@gmail.com>
2017-01-29 12:57:06 +0000

    Saves state in Board and re renders Square whenever a button is clicked

--- src/board.js
+++ src/board.js
@@ -2,8 +2,19 @@ import React, {Componenet} from 'react';
 import Square from './square'
 
 class Board extends React.Component {
+  constructor () {
+    super();
+    this.state = {
+      squares: Array(9).fill(null),
+    };
+  }
   renderSquare(i) {
-    return <Square value={i}/>;
+    return <Square value={this.state.squares[i]} onClick={() => this.handleClick(i)} />;
+  }
+  handleClick(i) {
+    const squares = this.state.squares.slice();
+    squares[i] = 'X';
+    this.setState({squares: squares});
   }
   render() {
     const status = 'Next player: X';
